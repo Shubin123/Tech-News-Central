83b331cfe07b2789008a21d5ff8cf4f44b0eda36
shubin123 <wangshubin130@gmail.com>
2019-02-13 22:01:10 -0800

    second commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+Tech articles, Done right
---
+++ assets/js/custom.js
@@ -0,0 +1,30 @@
+//clock
+function digi() {
+  var date = new Date(),
+      hour = date.getHours(),
+      minute = checkTime(date.getMinutes()),
+      ss = checkTime(date.getSeconds());
+
+  function checkTime(i) {
+    if( i < 10 ) {
+      i = "0" + i;
+    }
+    return i;
+  }
+
+if ( hour > 12 ) {
+  hour = hour - 12;
+  if ( hour == 12 ) {
+    hour = checkTime(hour);
+  document.getElementById("tt").innerHTML = hour+":"+minute+":"+ss+" AM";
+  }
+  else {
+    hour = checkTime(hour);
+    document.getElementById("tt").innerHTML = hour+":"+minute+":"+ss+" PM";
+  }
+}
+else {
+  document.getElementById("tt").innerHTML = hour+":"+minute+":"+ss+" AM";;
+}
+var time = setTimeout(digi,1000);
+}
